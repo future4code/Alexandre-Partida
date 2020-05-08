9bddbc68dea68ae102f324b7269200dcdeedba90
AlexandreGessone <alexandregessone@iMac-de-AlexandreGessone.local>
2020-05-08 13:15:31 -0300

    resolucao do exercício 2

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -8,4 +8,21 @@ A cotação do dólar deve ser digitada na tela e a conversão será feita atrav
 No código foi colocado o valor hipotético de 100 dólares. Imaginando que o dólar hoje esteja a 5,70, o código retornará 
 o valor de R$ 570,00.
 
+
+2.
+Na primeira parte do código, a função declarada investeDinheiro traz o resultado final do seu investimento depois de feita
+a conversão multiplicando o valor investido vezes a taxa correspondente. Para isso, é necessário escolher o tipo de investimento
+já que cada um apresenta uma taxa diferente.
+
+A variavel valorAposInvestimento retornará o valor total após a essa conversão.
+
+Ao final, o código mostrará duas opções de investimento. Na primeira, o usuário optou por investir 150 (novoMontante) em ações e na 
+segunda, investiu 200 em Tesouro Direto.
+
+A tela apresentará:
+
+para a variável novoMontante: 165
+para a variável segundoMontante: Tipo de investimento incorreto já que a opção Tesouro Direto não foi listada na função.
+
+
 */

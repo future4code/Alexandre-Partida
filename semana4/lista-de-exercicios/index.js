7f9ed8eab4c262e35bf5fe161a47d5213f990450
AlexandreGessone <alexandregessone@iMac-de-AlexandreGessone.local>
2020-05-08 12:54:37 -0300

    Resoluçao do exercício 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,11 @@
+/*Exercícios de interpretação de código
+
+1. 
+No código devemos informar a cotação do dólar e a quantidade de dólares que devem ser convertidos.
+O código retornará o valor equivalente em Reais.
+A cotação do dólar deve ser digitada na tela e a conversão será feita através da função declarada conversorDeMoeda.
+
+No código foi colocado o valor hipotético de 100 dólares. Imaginando que o dólar hoje esteja a 5,70, o código retornará 
+o valor de R$ 570,00.
+
+*/

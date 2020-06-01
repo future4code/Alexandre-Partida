c8df9308516f8ff8eb15b56708e2fe9cc9b8af12
Alexandre Gessone <alex.gessone@gmail.com>
2020-06-01 16:51:18 -0300

    segue o exercicio de hoje

---
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -0,0 +1,5 @@
+const despesas = {
+    valor:
+    tipo: ['casa', 'alimentação', 'viagem']
+    descrição:
+}
---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,59 @@
+/*Exercícios de interpretação de código
+
+1. 
+No código devemos informar a cotação do dólar e a quantidade de dólares que devem ser convertidos.
+O código retornará o valor equivalente em Reais.
+A cotação do dólar deve ser digitada na tela e a conversão será feita através da função declarada conversorDeMoeda.
+
+No código foi colocado o valor hipotético de 100 dólares. Imaginando que o dólar hoje esteja a 5,70, o código retornará 
+o valor de R$ 570,00.
+
+
+2.
+Na primeira parte do código, a função declarada investeDinheiro traz o resultado final do seu investimento depois de feita
+a conversão multiplicando o valor investido vezes a taxa correspondente. Para isso, é necessário escolher o tipo de investimento
+já que cada um apresenta uma taxa diferente.
+
+A variavel valorAposInvestimento retornará o valor total após a essa conversão.
+
+Ao final, o código mostrará duas opções de investimento. Na primeira, o usuário optou por investir 150 (novoMontante) em ações e na 
+segunda, investiu 200 em Tesouro Direto.
+
+A tela apresentará:
+
+para a variável novoMontante: 165
+para a variável segundoMontante: Tipo de investimento incorreto já que a opção Tesouro Direto não foi listada na função.
+
+3.
+O código verifica se um número é par ou ímpar e joga dentro de arrays diferentes. Se o número for par, vai para a array 1
+e se o número for ímpar, vai para a array 2
+
+No console será mostrado:
+"Quantidade total de números 14"
+6
+8
+
+4.
+Este código faz uma comparação com todos os números do array. Todos os números do array entram nas duas condições exceto o número
+-10. -10 é menor do que a variável numero1 mas não é maior do que a variavel numero2. Dessa forma, ele será mostrado apenas na 
+variavel numero1.
+
+No console será mostrado:
+25, 12, 55, 64, 121, 44, 11, 84, 51, 48, 14, 73, 111, 283, 1, 99, 13, 31, 83, 131, 1, 1,1, 25, 1590
+-10
+
+
+EXERCÍCIOS DE LÓGICA DE PROGRAMAÇÃO
+
+1.
+Através dos códigos "for", "for of" ou "push". 
+
+*/
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+
+numeros.push(9)
+
+console.log(numeros)
+
+

a8c052089770dd07ca4e7c020fccee2085b001a5
Alexandre Gessone <alex.gessone@gmail.com>
2020-07-27 19:41:17 -0300

    exercício restante

--- semana13/aula44/firstScript/src/Exercicios.ts
+++ semana13/aula44/firstScript/src/Exercicios.ts
@@ -81,10 +81,12 @@ const listeners: person = {
 
 
 EXERCÍCIO 2
-as entradas são os números dados pelo usuário e a saída são os números ordenados pelo maior, menor e a média.
-*/
 
-function obterEstatisticas(numeros: number[]): number {
+a)
+as entradas são os números dados pelo usuário e a saída são os números ordenados pelo maior, menor e a média.
+
+
+function obterEstatisticas(numeros: number[]): number[] {
 
     const numerosOrdenados: number[] = numeros.sort(
         (a: number, b: number) => a - b
@@ -95,20 +97,64 @@ function obterEstatisticas(numeros: number[]): number {
     for (let num of numeros) {
         soma += num
     }
-    type dadosEstatisticas = {
-        maior: number,
-        menor: number,
-        media: number
-    }
+   
+    
 
-    const estatisticas: dadosEstatisticas = {
+    const estatisticas: {maior: number, menor: number, media: number } = {
         maior: numerosOrdenados[numeros.length - 1],
         menor: numerosOrdenados[0],
         media: soma / numeros.length
     }
 
-    return estatisticas
+    return estatisticas 
 }
 
+b) compõem a função a variável estatisticas
 
+
+EXERCÍCIO 3
+
+a)
+
+type newPosts =  {
+        autor: string,
+        texto: string
+  } []
+
+  const posts: newPosts = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+
+
+
+b)
+entrada: autorInformado
+saída: post.autor
+*/
+
+  
+    
+
+
+
+  
 
